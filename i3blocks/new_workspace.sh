8a573c68914e696946fbd8ee0f78674abf91dab5
David Burkhart <David.Burkhart@gmx.de>
2019-11-17 17:38:34 +0100

    i3 update

--- i3blocks/new_workspace.sh
+++ i3blocks/new_workspace.sh
@@ -4,7 +4,7 @@ if [[ "$BLOCK_BUTTON" -eq 1 ]]; then
   i3-msg workspace $(($(i3-msg -t get_workspaces | jq ".[].num" | tail -1) + 1)) > /dev/null
 fi
 
-echo "new workspace"
-echo "new ws"
+echo "new"
+echo ""
 echo ""
 

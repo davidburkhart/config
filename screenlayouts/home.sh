9ca9f3d4552fef0276f19b8ec407a217325201a9
David Burkhart <David.Burkhart@gmx.de>
2021-04-02 12:42:33 +0200

    new screenlayouts; firefox

--- screenlayouts/home.sh
+++ screenlayouts/home.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-xrandr --dpi 192 --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output DP2-1 --mode 3840x2160 --pos 1920x0 --rotate normal --output DP2-2 --off --output DP2-3 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
+
+# USB
+# xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --mode 3840x2160 --pos 1920x0 --rotate normal --output DP2 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
+
+# Displayport
+xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-2-1 --primary --mode 3840x2160 --pos 1920x0 --rotate normal --output DP-2-2 --off --output DP-2-3 --off
+
+~/bin/set_dpi.sh 168
+
--- screenlayouts/only_docking_monitor.sh
+++ screenlayouts/only_docking_monitor.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --off --output DP2-1 --off --output DP2-2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+
+# USB
+# xrandr --output eDP-1 --off --output DP-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off
+
+# Displayport
+xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-2-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DP-2-2 --off --output DP-2-3 --off
+
+~/bin/set_dpi.sh 168
+
